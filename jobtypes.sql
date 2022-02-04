b2971062467a72025aabee4a7134ef14d2ffccc5
Michael Gibson <90262074+MGibson7@users.noreply.github.com>
2022-02-04 09:54:07 -0500

    Create jobtypes.sql

---
+++ jobtypes.sql
@@ -0,0 +1,6 @@
+USE viasatpayments;
+SELECT count(e.order_type), order_type
+FROM insertablehere e
+Where Technician_first_name = ''
+Group by order_type
+#MAKE SURE EXPORTING JOBS BY COMPLETED DATE
